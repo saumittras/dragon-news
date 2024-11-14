dcbe8ec27172525c1eb5b60c784913bfcb30a648
saumittras <saumittra.eee@gmail.com>
2024-11-14 10:33:12 +0600

    added grid main section

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -14,7 +14,11 @@ const HomeLayout = () => {
       <nav className="w-11/12 mx-auto">
         <Navbar></Navbar>
       </nav>
-      <main></main>
+      <main className="w-11/12 mx-auto pt-5 gap-6 grid md:grid-cols-12">
+        <aside className="left col-span-3">Left A side</aside>
+        <main className="main col-span-6">main contain</main>
+        <aside className="right col-span-3">Right A side</aside>
+      </main>
     </div>
   );
 };
